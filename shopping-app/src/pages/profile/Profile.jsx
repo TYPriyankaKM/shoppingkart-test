13a3b2782b5ef9e7a2f1ef46d86a80d7130c10e4
ubaid <wahidshams@gmail.com>
2022-07-28 17:55:30 +0530

    deleted some unnecessary files

--- shopping-app/src/pages/profile/Profile.jsx
+++ shopping-app/src/pages/profile/Profile.jsx
@@ -1,25 +1,9 @@
 import React, { useState } from "react";
-import avatar from "./profile.module.css";
-import Locations from "./Locations";
 
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
 import "./profile.css";
-import { useDispatch } from "react-redux";
-import MEN from "../../menprofile.png";
-import Axios from "../../apis/Axios";
 
-import { createCurrentUser } from "../../features/User/userSlice";
-import {
-  Avatar,
-  Box,
-  Button,
-  Container,
-  Stack,
-  Typography,
-} from "@mui/material";
-import ProfileInfo from "./ProfileInfo";
+import { Container } from "@mui/material";
+
 import ProfileOutlet from "./ProfileOutlet";
 import UserDashBoard from "./UserDashBoard";
 const Profile = () => {
